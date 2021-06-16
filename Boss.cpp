d26a7efd75e6e35850182cff68b1d8f38f6847c8
aliakhlaqi <aliakhlaqi9898@gmail.com>
2021-06-17 03:14:59 +0430

    edite 1

---
+++ Boss.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aliakhlaqi on 6/17/2021 AD.
+//
+
+#include "Boss.h"
---
+++ Boss.h
@@ -0,0 +1,16 @@
+//
+// Created by aliakhlaqi on 6/17/2021 AD.
+//
+
+#ifndef HW6_2_BOSS_H
+#define HW6_2_BOSS_H
+
+
+
+class Boss {
+
+};
+
+
+
+#endif //HW6_2_BOSS_H
